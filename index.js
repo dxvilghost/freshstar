142da9f4215d18bac77127b0c802c5152efb844c
alpha <56209673+alphaxdd@users.noreply.github.com>
2022-03-10 12:46:21 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
-
-const express = require("express"),
-    app = express(),
-    ejs = require('ejs'),
-    client = new (require("discord.js")).Client()
+const app = require("express")()
+const client = new (require("discord.js")).Client()
 
 client.on("ready", async () => {
     console.log('[BOT] Iniciado')
     await client.user.setStatus("invisible");
 });
 
-
 app.set("view engine", "ejs");
 app.use(express.static("public"))
-
-
 
 
 app.use(function (req, res, next) {
@@ -22,60 +16,33 @@ app.use(function (req, res, next) {
 });
 
 app.post("*", async (req, res) => {
-   return res.send('foi mal mano nao vai rolar request aq')
+    return res.send('foi mal mano nao vai rolar request aq')
 });
 
 
 app.get("/", async (req, res, next) => {
-  const yogurt = await client.users.fetch
-  ("230113935351611392")
-  const rep7 = await client.users.fetch
-  ("330879828683390976")
- // const emoji = await client.channels.fetch
-//  ("947627696861622293")
-  const server = await client.guilds.fetch("642499335858290701")
+    const yogurt = await client.users.fetch("230113935351611392")
+    const rep7 = await client.users.fetch("330879828683390976")
+    const server = await client.guilds.fetch("642499335858290701")
+    const role = await server.roles.fetch('944298404375322664');
+    
+    await server.members.fetch();
+    const { members } = role
+    let values = ["4ngp7pWwiJ0", "NYX6e7X3jQ4", "rUWMh-GPY0M", "ZZlgs4FB3tg", "i0hONpMvNVU"];
+    let valueToUse = values[Math.floor(Math.random() * values.length)];
 
-
-
-  //const membros = server.roles.fetch('944298404375322664').members.map(m=>m.user.tag);
-  //console.log(membros);
-
-const role = await server.roles.fetch('944298404375322664');
-await server.members.fetch();
-const { members } = role
-
-//members.forEach(async (obj) => {
-  //const response = await client.users.fetch
- // (obj.user.id)
-//})
-
-
-var values = ["4ngp7pWwiJ0","NYX6e7X3jQ4","rUWMh-GPY0M","ZZlgs4FB3tg","i0hONpMvNVU"],
-valueToUse = values[Math.floor(Math.random() * values.length)];
-
-   
-
-	return res.redirect('https://xvideos.com')
-/// return res.render("index", { yogurt, rep7, members, server, valueToUse  });
-	
-	
-	
-	
-	
+    return res.render("index", { yogurt, rep7, members, server, valueToUse  });
 });
 
 
-
-
-app.use(function (req,res){
-	res.status(404).render('404');
+app.use((req, res)  => {
+    res.status(404).render('404');
 });
 
 client.login(process.env['TOKEN']);
 
 app.listen(process.env.PORT, () => {
-    console.log('[EXPRESS] Iniciado') 
+    console.log('[EXPRESS] Iniciado')
 });
-
 
 setTimeout(() => process.exit(), 600000)
